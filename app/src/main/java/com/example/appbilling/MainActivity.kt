f50b3948e0d3851becfddc44e48ef0c9eff96952
Gregorioous <MacGregorKing@yandex.ru>
2023-04-02 05:47:26 +0300

    new version

--- app/src/main/java/com/example/appbilling/MainActivity.kt
+++ app/src/main/java/com/example/appbilling/MainActivity.kt
@@ -1,49 +1,59 @@
 package com.example.appbilling
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.telecom.Call.Details
-import android.util.Log
-import android.view.SurfaceControl.Transaction
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.anjlab.android.iab.v3.BillingProcessor
-import com.anjlab.android.iab.v3.PurchaseInfo
+import com.android.billingclient.api.*
 import com.example.appbilling.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(),BillingProcessor.IBillingHandler {
-    private val TAG = MainActivity::class.java.simpleName
-    private var bp: BillingProcessor? = null
-    private var binding:ActivityMainBinding? = null
+class MainActivity : AppCompatActivity() {
+   private lateinit var binding:ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-        bp = BillingProcessor.newBillingProcessor(this,"здесь должен быть ключ ",this)
-        bp?.initialize()
+        val skuList = ArrayList<String>()
+        skuList.add("test_prod_1")
 
-        binding?.buttonFirst?.setOnClickListener{
-            bp?.purchase(this@MainActivity, "android.test.purchased")
+        val purchasesUpdatedListener = PurchasesUpdatedListener{
+                billingResult, purchses ->
         }
 
-    }
+        var billingClient = BillingClient.newBuilder(this)
+            .setListener(purchasesUpdatedListener)
+            .enablePendingPurchases().build()
 
+        binding.button.setOnClickListener {
 
-    override fun onProductPurchased(productId: String, details: PurchaseInfo?) {
-        Toast.makeText(applicationContext,"ID of product $productId, details ${details?.purchaseData?.purchaseState}", Toast.LENGTH_SHORT).show()
-    }
+            billingClient.startConnection(object : BillingClientStateListener {
+                override fun onBillingServiceDisconnected() {
+//                    TODO("Not yet implemented")
+                }
 
+                override fun onBillingSetupFinished(billingResult: BillingResult) {
+//                    TODO("Not yet implemented")
 
-    override fun onPurchaseHistoryRestored() {
-        TODO("Not yet implemented")
-    }
+                    if (billingResult.responseCode == BillingClient.BillingResponseCode.OK){
 
-    override fun onBillingError(errorCode: Int, error: Throwable?) {
-        Toast.makeText(applicationContext,"error ${error?.localizedMessage}, code $errorCode", Toast.LENGTH_LONG).show()
-    }
+                        val params = SkuDetailsParams.newBuilder()
+                        params.setSkusList(skuList)
+                            .setType(BillingClient.SkuType.INAPP)
 
-    override fun onBillingInitialized() {
-        Toast.makeText(applicationContext,"bp insializated", Toast.LENGTH_SHORT).show()
-        Log.e(TAG,"biling inited")
+                        billingClient.querySkuDetailsAsync(params.build()){
+                                billingResult, skuDetailsList ->
+
+                            for (skuDetails in skuDetailsList!!) {
+                                val flowPurchase = BillingFlowParams.newBuilder()
+                                    .setSkuDetails(skuDetails)
+                                    .build()
+
+                                val responseCode = billingClient.launchBillingFlow(this@MainActivity, flowPurchase).responseCode
+                            }
+                        }
+                    }
+                }
+
+            })
+        }
     }
 }
